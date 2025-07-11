ddc6c59314b304b89facae5e944eebb914aeb96a
mathilde lebouil @users.noreply.github.com <lebouilmathilde@gmail.com>
2025-07-11 16:12:55 +0200

    ideas and pundit

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,70 +1,58 @@
 class IdeasController < ApplicationController
-  before_action :set_idea, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_idea, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_idea, only: [:edit, :update, :destroy]
 
-  # GET /ideas or /ideas.json
   def index
-    @ideas = Idea.all
+    @ideas = policy_scope(Idea.all)
   end
 
-  # GET /ideas/1 or /ideas/1.json
   def show
   end
 
-  # GET /ideas/new
   def new
-    @idea = Idea.new
+    @idea = current_user.ideas.build
+    authorize @idea
   end
 
-  # GET /ideas/1/edit
-  def edit
-  end
-
-  # POST /ideas or /ideas.json
   def create
-    @idea = Idea.new(idea_params)
+    @idea = current_user.ideas.build(idea_params)
+    authorize @idea
 
-    respond_to do |format|
-      if @idea.save
-        format.html { redirect_to @idea, notice: "Idea was successfully created." }
-        format.json { render :show, status: :created, location: @idea }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
+    if @idea.save
+      redirect_to @idea, notice: "Idée créée avec succès !"
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /ideas/1 or /ideas/1.json
+  def edit; end
+
   def update
-    respond_to do |format|
-      if @idea.update(idea_params)
-        format.html { redirect_to @idea, notice: "Idea was successfully updated." }
-        format.json { render :show, status: :ok, location: @idea }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
+    if @idea.update(idea_params)
+      redirect_to @idea, notice: "Idée mise à jour."
+    else
+      render :edit
     end
   end
 
-  # DELETE /ideas/1 or /ideas/1.json
   def destroy
-    @idea.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to ideas_path, status: :see_other, notice: "Idea was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @idea.destroy
+    redirect_to ideas_path, notice: "Idée supprimée."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_idea
-      @idea = Idea.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def idea_params
-      params.require(:idea).permit(:title, :description, :user_id)
-    end
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  def authorize_idea
+    authorize @idea
+  end
+
+  def idea_params
+    params.require(:idea).permit(:title, :description)
+  end
 end
+
